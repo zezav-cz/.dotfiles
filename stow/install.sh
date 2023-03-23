ed1775be85013367e8e7d660c897eb0402f6fb63
Jan Trojak <trojakjan24@gmail.com>
2023-03-23 17:25:13 +0100

    update

--- stow/install.sh
+++ stow/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+cd `dirname $0`
+
 
 for dir in */ ; do
     echo "setting up: $dir"
     stow -R -t ~ $dir
 done
+
+cd - > /dev/null
 
 exit 0
--- stow/uninstall.sh
+++ stow/uninstall.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+cd `dirname $0`
+
 for dir in */ ; do
     echo "removing: $dir"
     stow -D $dir
 done
+
+cd - > /dev/null
+
 
 exit 0
